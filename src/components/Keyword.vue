e94f734bb5ba954c43a57d17df027e29b9a5fbd8
Johannes Thiel <johannes.thiel@uni-duesseldorf.de>
2019-02-08 00:02:37 +0100

    Keyword rendering now display colors the keyword

--- src/components/Keyword.vue
+++ src/components/Keyword.vue
@@ -1,11 +1,24 @@
 <template lang="pug">
   .keyword
-    p {{ keyword.text }}
+    p 
+      div(v-html='keywordHtml')
 </template>
 
 <script>
 export default {
   name: 'Keyword',
+  computed: {
+    keywordHtml () {
+      // Matches the opening color tag, the text within the tag and the closing tag.
+      // Groups the hex-code including the '#' and the tag name.
+      // See https://regex101.com/r/GpBkAc/1 for  a explaination of the regExp.
+      const regExp = /<(#\d{6})>(\w+:)<\/color>/
+      // Replace the color tag with a span elemnt that uses the matched color and keyword name.
+      const replacementString = '<span style="color: $1;"> $2 </span>'
+
+      return this.keyword.raw.replace(regExp, replacementString)
+    },
+  },
   props: ['keyword'],
 }
 </script>
