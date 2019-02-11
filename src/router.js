6602df151e645ef97779ff638ecdce44f1b81b2d
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-02-11 23:09:27 +0100

    Keyword Component (#5)
    * Basic keyword component
    
    * Keyword rendering now display colors the keyword
    
    * Keyword component tests

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import Cards from '@/views/Cards.vue'
 
+import Test from '@/views/Test.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -18,6 +20,11 @@ export default new Router({
       path: '/cards',
       name: 'cards',
       component: Cards,
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test,
     },
   ],
 })
---
+++ src/views/Test.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .test
+    Keyword(:keyword='testKeyword') 
+</template>
+
+<script>
+import Keyword from '@/components/Keyword.vue'
+import testKeyword from '@/../tests/unit/test-data/test-keyword.json'
+import languageHelper from '@/util/language-helper'
+
+export default {
+  name: 'Test',
+  computed: {
+    testKeyword () {
+      return languageHelper.translateKeyword(testKeyword, 'en-US')
+    },
+  },
+  components: {
+    Keyword,
+  },
+}
+</script>
