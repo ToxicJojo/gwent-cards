751e0f1dce880eb51a240358caeae248f53ce925
Johannes Thiel <johannes.thiel@uni-duesseldorf.de>
2019-02-19 19:46:38 +0100

    Basic card preview

---
+++ src/components/CardPreview.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .card-preview(@click='$emit("click")')
+    img(:src='card.variations[card.ingameId + "00"].art.low')
+</template>
+
+<script>
+export default {
+  name: 'CardPreview',
+  props: ['card']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
