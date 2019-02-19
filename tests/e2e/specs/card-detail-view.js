9b6262af88f294ffda17517d77be024be1654e9d
Johannes Thiel <johannes.thiel@uni-duesseldorf.de>
2019-02-19 18:52:36 +0100

    Integration tests

---
+++ tests/e2e/specs/card-detail-view.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress" />
+
+describe('Card Detail View', () => {
+  before(() => {
+    cy.visit('/cards/112102')
+  })
+
+  it('shows cards name', () => {
+    cy.contains('Geralt: Igni')
+  })
+
+  it('shows card type', () => {
+    cy.contains('Witcher')
+  })
+
+  it('show card ability', () => {
+    cy.contains('Destroy the highest unit(s) on an enemy row with a total of 20 or more power.')
+  })
+
+  it('shows card flavor', () => {
+    cy.contains('A twist of a witcher\'s fingers can light a lamp… or incinerate a foe.')
+  })
+
+  it('shows keyword explaination', () => {
+    cy.contains('Trigger this ability when played.')
+    cy.contains('This ability can only be used while on the melee row.')
+  })
+})
