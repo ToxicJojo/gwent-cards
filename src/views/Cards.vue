30d63ea8408ca9671e5e72dc3b4f08831d16997c
Johannes Thiel <johannes.thiel@uni-duesseldorf.de>
2019-02-21 19:23:42 +0100

    Simple card list

--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -2,18 +2,12 @@
   div#view-cards
     h1 Cards
     router-view(:cards='cards' :categories='categories' :keywords='keywords')
-    CardPreview(:card='cards["112102"]')
 </template>
 
 <script>
-import CardPreview from '@/components/CardPreview.vue'
-
 export default {
   name: 'Cards',
   props: ['cards', 'categories', 'keywords'],
-  components: {
-    CardPreview,
-  },
 }
 </script>
 
