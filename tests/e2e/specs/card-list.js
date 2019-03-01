1fe928d4b70895b2dbedfdad674cce38a5890fa5
Johannes Thiel <johannes.thiel@uni-duesseldorf.de>
2019-03-01 18:46:29 +0100

    Integration tests for faction filter

--- tests/e2e/specs/card-list.js
+++ tests/e2e/specs/card-list.js
@@ -7,12 +7,38 @@ describe('Card Detail View', () => {
 
   it('shows card list', () => {
     // The amount of cards will change. So we only check for a minimum amount.
-    const expectecMinimumCardCount = 100
+    const expectecMinimumCardCount = 400
     cy.get('.card-list').children().should('have.length.greaterThan', expectecMinimumCardCount)
   })
 
   it('clicking a card opens the detail view', () => {
     cy.get('.card-list').children().first().click()
     cy.location('pathname').should('equal', '/cards/112101')
+    cy.go('back')
+  })
+
+  describe('Card Filter', () => {
+    it('filters cards according to factions', () => {
+      cy.get('.faction-select').get('input').eq(1).click()
+      cy.get('.card-list').children().should('have.length.lessThan', 200)
+
+      cy.get('.faction-select').get('input').eq(2).click()
+      cy.get('.card-list').children().should('have.length.lessThan', 200)
+
+      cy.get('.faction-select').get('input').eq(3).click()
+      cy.get('.card-list').children().should('have.length.lessThan', 200)
+
+      cy.get('.faction-select').get('input').eq(4).click()
+      cy.get('.card-list').children().should('have.length.lessThan', 200)
+
+      cy.get('.faction-select').get('input').eq(5).click()
+      cy.get('.card-list').children().should('have.length.lessThan', 200)
+
+      cy.get('.faction-select').get('input').eq(6).click()
+      cy.get('.card-list').children().should('have.length.lessThan', 200)
+
+      cy.get('.faction-select').get('input').eq(0).click()
+      cy.get('.card-list').children().should('have.length.greaterThan', 400)
+    })
   })
 })
