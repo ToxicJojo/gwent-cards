be8687824b34fda00f9ad7ca7b07eed24ac6fe00
Johannes Thiel <johannes.thiel@uni-duesseldorf.de>
2019-04-19 15:51:20 +0200

    Updated to newset vue/cli version

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,12 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "@/scss/variables/_all.scss";`,
+        data: '@import "@/scss/variables/_all.scss";',
       },
     },
   },
+
+  productionSourceMap: false,
+  parallel: false,
+  publicPath: '',
 }
