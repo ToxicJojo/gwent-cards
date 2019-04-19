40f4dcdc5b823642f2feb140ef6cb96836e5c64f
Johannes Thiel <johannes.thiel@uni-duesseldorf.de>
2019-04-19 23:13:31 +0200

    Adde deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+#set -e
+
+# build
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:ToxicJojo>/gwent-cards.git master:gh-pages
+
